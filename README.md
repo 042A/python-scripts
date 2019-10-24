ee20ec533c0faeaaf0fdb04dd654c00e6c8580bd
Johan Ahlstrand <042ahlstrand@gmail.com>
2019-10-24 19:53:33 +0200

    Added mouselogger

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Just some examples how to convert to and from UNIX-time
 
 ## Web Automation
 A small example of how selenium libary works, with use of a headless browser    
+
+## Mouselogger
+Use of https://pypi.org/project/pynput/ to log mouse clicks. The libary supports both keyboard and mouse and might be used for macros etc.
---
+++ mouse-logger.py
@@ -0,0 +1,18 @@
+from pynput import mouse
+
+class MyException(Exception):pass
+
+NumberOfMouseClicks = 0
+
+def on_click(x, y, button, pressed):
+    global NumberOfMouseClicks
+    print(x, y)
+    NumberOfMouseClicks = NumberOfMouseClicks + 1
+    if (NumberOfMouseClicks==10):
+        raise MyException(button)
+
+with mouse.Listener(on_click=on_click) as listener:
+    try:
+        listener.join()
+    except MyException as e:
+        pass
